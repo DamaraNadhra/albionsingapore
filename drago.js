b99024be7182a9dca9f446e8a67568f98e37cf16
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-06-03 10:06:58 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -494,23 +494,26 @@ client.on('message', async (message) => {
         .setDescription(`**ERROR** \nThis command is disabled in this channel to prevent clutter, please redo this command at ${channel}`)
         .setFooter('If this is wrong please contact the officers :D')
         console.log(message.channel.id)
-        if (message.channel.id !== '779514684797091850' | message.channel.id !== '760731834354499585') return message.channel.send(embedz)
-        let firstArgument = args[0];
-        if (!firstArgument) return message.reply('Please state the person you want to check!')
-        axios.get(`https://api.aotools.net/v2/blacklist/${firstArgument}`)
-        .then(async result => {
-            console.log(result.data.isBlacklisted)
-            if (result.data.isBlacklisted === true) {
-                const embed = new Discord.MessageEmbed()
-                .setColor('AQUA')
-                .setAuthor('Singapore Police', 'https://cdn.discordapp.com/icons/703862691608920114/669f0e6605601754a64fbb829ede2c00.webp?size=256')
-                .setDescription('**This player is blacklisted!** Please dont invite him over to the guild or just kick him directly! Please look into ARCH main discord for more info')
-                .setFooter('If this is wrong please contact the officers :D')
-                message.channel.send(embed)
-            } else {
-                message.reply(`${result.data.name} is not blacklisted :D`)
-            }
-        })
+        if (message.channel.id === '779514684797091850' | message.channel.id === '760731834354499585') {
+            let firstArgument = args[0];
+            if (!firstArgument) return message.reply('Please state the person you want to check!')
+            axios.get(`https://api.aotools.net/v2/blacklist/${firstArgument}`)
+            .then(async result => {
+                console.log(result.data.isBlacklisted)
+                if (result.data.isBlacklisted === true) {
+                    const embed = new Discord.MessageEmbed()
+                    .setColor('AQUA')
+                    .setAuthor('Singapore Police', 'https://cdn.discordapp.com/icons/703862691608920114/669f0e6605601754a64fbb829ede2c00.webp?size=256')
+                    .setDescription('**This player is blacklisted!** Please dont invite him over to the guild or just kick him directly! Please look into ARCH main discord for more info')
+                    .setFooter('If this is wrong please contact the officers :D')
+                    message.channel.send(embed)
+                } else {
+                    message.reply(`${result.data.name} is not blacklisted :D`)
+                }
+            })
+        } else {
+            return message.channel.send(embedz)
+        }
     }
 })
 client.on('ready', () => {
