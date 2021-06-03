02f4744de6b45a376560b9023bdba742608d1f5b
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-06-03 10:04:53 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -493,6 +493,7 @@ client.on('message', async (message) => {
         .setAuthor('Singapore Police', 'https://cdn.discordapp.com/icons/703862691608920114/669f0e6605601754a64fbb829ede2c00.webp?size=256')
         .setDescription(`**ERROR** \nThis command is disabled in this channel to prevent clutter, please redo this command at ${channel}`)
         .setFooter('If this is wrong please contact the officers :D')
+        console.log(message.channel.id)
         if (message.channel.id !== '779514684797091850' | message.channel.id !== '760731834354499585') return message.channel.send(embedz)
         let firstArgument = args[0];
         if (!firstArgument) return message.reply('Please state the person you want to check!')
