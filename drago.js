db86b04ee203a0ba4d7c0df87da4765a6fa0d060
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-06-13 15:46:30 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -542,129 +542,131 @@ client.on('message', async (message) => {
             message.channel.send('Life is hard, but you must keep going <:godbless:824231355331510274> \n**MightyG2021**')
          }
     } else if (command === 'handle') {
-        if (message.member.roles.cache.has('759793776439984170')) {
-            await mongo().then(async mongoose => {
-                if (message.channel.parentID === '853522303811321876') {
-                    let isHandled = await report.findOne({ officerId: message.author.id})
-                    if (isHandled) {
-                        return message.author.send(`I'm sorry but ${message.guild.members.cache.get(isHandled.officerId).nickname} is handling this case`)
-                    } else {
-                        if (message.author.id === '694488949980135444') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`gremory${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-                                
-                            })
-                        } else if (message.author.id === '382507136883621889') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`luai${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        } else if (message.author.id === '412598799081406465') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`amorkia${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        } else if (message.author.id === '229238295257677835') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`fatmeow${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        } else if (message.author.id === '590791325759045643') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`gnamo${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        } else if (message.author.id === '409717155035217922') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`lightdragneel${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        } else if (message.author.id === '263996887131095041') {
-                            const ticketNumber = message.channel.name.slice('ticket'.length);
-                            const channel = message.guild.channels.cache.get('779514684797091850');
-                            const channelID = message.channel.id;
-                            message.channel.setName(`lightdragneel${ticketNumber}`)
-                            channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
-                            message.delete()
-                            console.log(ticketNumber)
-                            await report.create({
-                                officer: message.guild.members.cache.get(message.author.id).nickname,
-                                officerId: message.author.id,
-                                channelId: message.channel.id,
-                                status: 'Ongoing'
-
-                            })
-                        }
-                    }
+        await mongo().then(async mongoose => {
+            if (message.member.roles.cache.has('759793776439984170')) {
+            if (message.channel.parentID === '853522303811321876') {
+                let isHandled = await report.findOne({ officerId: message.author.id})
+                if (isHandled) {
+                    return message.author.send(`I'm sorry but ${message.guild.members.cache.get(isHandled.officerId).nickname} is handling this case`)
                 } else {
-                    message.delete()
-                    return message.author.send('This command is used to handle cases!')
+                    if (message.author.id === '694488949980135444') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`gremory${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+                            
+                        })
+                    } else if (message.author.id === '382507136883621889') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`luai${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    } else if (message.author.id === '412598799081406465') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`amorkia${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    } else if (message.author.id === '229238295257677835') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`fatmeow${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    } else if (message.author.id === '590791325759045643') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`gnamo${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    } else if (message.author.id === '409717155035217922') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`lightdragneel${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    } else if (message.author.id === '263996887131095041') {
+                        const ticketNumber = message.channel.name.slice('ticket'.length);
+                        const channel = message.guild.channels.cache.get('779514684797091850');
+                        const channelID = message.channel.id;
+                        message.channel.setName(`lightdragneel${ticketNumber}`)
+                        channel.send(`<@${message.author.id}> is handling case <#${channelID}>`)
+                        message.delete()
+                        console.log(ticketNumber)
+                        await report.create({
+                            officer: message.guild.members.cache.get(message.author.id).nickname,
+                            officerId: message.author.id,
+                            channelId: message.channel.id,
+                            status: 'Ongoing'
+
+                        })
+                    }
                 }
-            })
+            } else {
+                message.delete()
+                return message.author.send('This command is used to handle cases!')
+            }
         } else {
             message.delete()
             return message.author.send('I\'m sorry but you don\'t have the right to use this command')
         }
+        })
+        
+            
     }
 }) 
 const emojis = {
@@ -740,7 +742,7 @@ client.on('guildMemberAdd', (member) => {
    **Fatmeow#3662**`)
 })
 
-client.on('channelDelete', (channel) => {
+client.on('channelDelete',async  (channel) => {
     const channelId = channel.id
     await mongo().then(async mongoose => {
         await report.findOneAndDelete({ channelId: channelId})
