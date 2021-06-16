e6f046f80ade448e7c47a830ce6a0d1bce2581d4
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-06-16 09:34:49 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -681,7 +681,7 @@ client.on('message', async (message) => {
             const embed = new Discord.MessageEmbed()
             .setAuthor('Killboard Info', robot.user.displayAvatarURL())
             .setColor('BLUE')
-            .setDescription(`List to fast check killboard \n[Killboard link](https://albiononline.com/en/killboard/kill/${eventId})`)
+            .setDescription(`List to fast check killboard \n[Killboard link](https://albiononline.com/en/killboard/kill/${eventId}) \n**Deathfame:** ${event.KillFame}`)
             .addFields(
                 {name: '__**Victim**__', value: `**Name:** ${event.Victim.Name} \n**Average IP:** ${Math.round(event.Victim.AverageItemPower)} \n**Guild:** ${event.Victim.GuildName} \n**Aliance:** ${event.Victim.AllianceName}`, inline: true},
                 { name: '__**Killer**__', value: `**Name:** ${event.Killer.Name} \n**Average IP:** ${Math.round(event.Killer.AverageItemPower)} \n**Guild:** ${event.Killer.GuildName} \n**Alliance:** ${event.Killer.AllianceName}`, inline: true}
@@ -690,6 +690,8 @@ client.on('message', async (message) => {
             .setTimestamp(new Date())
             message.channel.send(embed)
             console.log(event)
+        }).catch(e => {
+            message.channel.send('Cannot find a killboard with that id')
         })
     }
 }) 
