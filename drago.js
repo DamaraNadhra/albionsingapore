e5a5577cfd90d350c1f588f6b8d3b340f99eecd2
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-06-19 16:53:07 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -868,15 +868,24 @@ client.on('message', async (message) => {
             message.channel.send('Cannot find a killboard with that id')
         })
     } else if (command === 'add') {
-        let personMention = message.mentions.members.first()
-        let channelMention = message.mentions.channels.first()
-        if (!personMention) return message.reply(`Please state the person's mention`)
-        if (!channelMention) return message.reply(`Please state the channel's mention`)
-        channelMention.updateOverwrite(personMention.user, {
-            SEND_MESSAGES: true,
-            VIEW_CHANNEL: true
-        })
-        message.reply(`${personMention} has been added to ${channelMention}`)
+        if (message.member.roles.cache.has('759793776439984170') | message.member.roles.cache.has('855688610782248980') | message.member.roles.cache.has('855689169018814464')) {
+            let personMention = message.mentions.members.first()
+            let channelMention = message.mentions.channels.first()
+            message.delete()
+            if (!personMention) return message.reply(`Please state the person's mention`)
+            if (!channelMention) return message.reply(`Please state the channel's mention`)
+            if (channelMention.parentID === '853522303811321876' | channelMention.parentID === '853522605684686878') {
+                channelMention.updateOverwrite(personMention.user, {
+                    SEND_MESSAGES: true,
+                    VIEW_CHANNEL: true
+                })
+                message.reply(`${personMention} has been added to ${channelMention}`)
+            } else {
+                message.reply('This command only works for ticket channels, thank you', '<:jennielove:844893922634235904>')
+            }
+        } else {
+            message.reply('You don\'t have permission to use this command')
+        }
     } else if (command === 'checkbattle') {
         let battleId = args[0];
         if (!battleId) return message.reply('You must state the battle Id!')
