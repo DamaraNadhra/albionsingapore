702053a3d419af2f4dbbe8eec4dd17eac3d665ef
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-07-05 23:40:55 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -1310,11 +1310,11 @@ client.on('message', async (message) => {
         .setStyle('DANGER')
         let embed = new MessageEmbed()
         .setColor('ORANGE')
-        .setAuthor('Singapore Ava Slave', robot.user.displayAvatarURL())
+        .setAuthor('Singapore Ava Slave', client.user.displayAvatarURL())
         .setTitle('Introduction...')
         .setDescription(`Avalonian Elite Dungeon is a difficult dungeon which can only be finished with 20 men. \nYou can only find the **Avalonian Elite Dungeon** around the outlands, you can also find **Avalonian Elite Dungeon** inside the Avalonian Roads as well. The tier starts from T6. \n\nHowever, finding a natural spawn dungeon is a little bit difficult, that's why people used to pop it from a map. \nIn Singapore guild we always do **8.2+ Avalonian Elite Dungeon** that's why you are expected to wear **8.3 gears** and hit at least **1650 IP**. \n\nAs for the shape of the dungeon entrance is like the image below `)
         .setImage('https://i.imgur.com/vjQoBtm.png')
-        .setFooter('Singapore', robot.user.displayAvatarURL())
+        .setFooter('Singapore', client.user.displayAvatarURL())
         message.channel.send({
             embeds: [embed],
             components: [[listButton, closeButton]]
@@ -1482,11 +1482,11 @@ client.on('interaction',async  interaction => {
         .setStyle('DANGER')
         let embed = new MessageEmbed()
         .setColor('ORANGE')
-        .setAuthor('Singapore Ava Slave', robot.user.displayAvatarURL())
+        .setAuthor('Singapore Ava Slave', client.user.displayAvatarURL())
         .setTitle('Introduction...')
         .setDescription(`Avalonian Elite Dungeon is a difficult dungeon which can only be finished with 20 men. \nYou can only find the **Avalonian Elite Dungeon** around the outlands, you can also find **Avalonian Elite Dungeon** inside the Avalonian Roads as well. The tier starts from T6. \n\nHowever, finding a natural spawn dungeon is a little bit difficult, that's why people used to pop it from a map. \nIn Singapore guild we always do **8.2+ Avalonian Elite Dungeon** that's why you are expected to wear **8.3 gears** and hit at least **1650 IP**. \n\nAs for the shape of the dungeon entrance is like the image below `)
         .setImage('https://i.imgur.com/vjQoBtm.png')
-        .setFooter('Singapore', robot.user.displayAvatarURL())
+        .setFooter('Singapore', client.user.displayAvatarURL())
             interaction.update({
                 embeds: [embed],
                 components: [[listButton, closeButton]]
