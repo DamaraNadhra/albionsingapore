ccec014775bc7251ba26b6d3b3fc87014519c1d1
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-07-07 12:54:46 +0700

    Update drago.js

--- drago.js
+++ drago.js
@@ -1381,6 +1381,16 @@ client.on('message', async (message) => {
             components: [[listButton, closeButton]]
         })
     }
+    } else if (command === 'approved-scout') {
+        let embed = new MessageEmbed()
+        .setAuthor('Approved Scout', 'https://render.albiononline.com/v1/item/T6_MAIN_ROCKMACE_KEEPER.png')
+        .setColor('ORANGE')
+        .setDescription(`**Approved Scout** is a title given to trusted and tested scout, in order to be an **approved scout**, you must **follow the steps** explained below: \n\n**1.** Wear build that can buy time for the party to port out from the dungeon once the **divers/gankers** came in. (build example will show below) \n**2.** You must report the situation **regularly*8 to the party member. \n**3.** Turn on **mastery volume and combat log**, so you can hear gankers steps and whenever they pick stuff. \n**4.** After you have followed all the steps, contact one of the officers and send **a screenshot of your scout build**. \n**5.** The officer will add you into the **ARCH approved scout List** \n\n**Note:** \n**1.** If let's say your party got doved by divers. You are not responsible to regear / compensate the deaths, but they have the right to not invite you again in the future. \n**2.** If gankers came in, and you died when you are trying to hold them, it's **mandatory** to regear and come back to the dungeon asap. \n**3.** Not completing the task **means no PAYMENT**`)
+        .setImage('https://i.imgur.com/eCq54NK.jpg')
+        .setFooter('Singapore ontop baby', client.user.displayAvatarURL())
+        message.channel.send({
+            embeds: [embed]
+        })
     } Object.keys(avalist).forEach((m, i) => {
         if (command.includes(m)) {
             let botCommandChannel = message.guild.channels.cache.get('760731834354499585')
