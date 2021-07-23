63b453d623098f69391bd7b14c640a651c70ce33
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-07-23 11:28:15 +0700

    fixing something

--- commands/rename.js
+++ commands/rename.js
@@ -1,12 +1,12 @@
 const { Message } = require("discord.js");
 const rep = require("./models/reputation");
 module.exports = {
-  name: "blacklist",
+  name: "rename",
   description: "Blacklists people from joining Singapore Guild",
   /**
    * @param {Message} message
    */
-  execute(message, args) {
+  async execute(message, args) {
     const blabla = await rep.findOne({ name: "ChengPoh" });
     message.channel.send(blabla.rep);
   },
