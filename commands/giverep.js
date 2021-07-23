53dc35263d8e88d34e04f30e15d41e5bd12d7cfa
DamaraNadhra <70007198+DamaraNadhra@users.noreply.github.com>
2021-07-23 14:40:49 +0700

    Update giverep.js

--- commands/giverep.js
+++ commands/giverep.js
@@ -1,6 +1,7 @@
 const rep = require("../models/reputation");
 const { nicknameMaker, dateMaker } = require("../functions");
 const { recentlyRan } = require("../cooldown");
+const { MessageButton, MessageEmbed }
 module.exports = {
   name: "giverep",
   description: "gives reputation to provided player!",
